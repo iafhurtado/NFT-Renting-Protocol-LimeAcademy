d9232b0b9b7c00f9f7b01f1d469bf43ce0cd605b
JaiBo <65864145+iafhurtado@users.noreply.github.com>
2021-11-29 18:14:21 +0100

    Update Marketplace.sol

--- Marketplace.sol
+++ Marketplace.sol
@@ -1,6 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.2;
 
+contract EscrowState {
+     mapping(uint => address) public approvedRenters;
+
+    // 
+    mapping(address => mapping(address => uint)) public ListedNFTsIds;
+
+    // 
+    mapping(address => address) public requestsToBeOperator;
+    
+    event UserDeposit(address indexed user, address indexed asset, uint amount);
+    event UserWithdraw(address indexed user, address indexed asset, uint amount);
+}
 
 contract Marketplace {
+    function deposit(address nonFungibleTokenID) external onlyRole(OWNER && OPERATOR) {
+                   emit HouseRegistered(houseAddress, hOfCoin.HOUSE_TYPE(), asset);
+    
+
 }
