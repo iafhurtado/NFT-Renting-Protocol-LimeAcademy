7c631796ca1446048b46da8fc40fe1a9fb31ca94
JaiBo <65864145+iafhurtado@users.noreply.github.com>
2021-11-30 10:48:31 +0100

    Update Marketplace.sol

--- Marketplace.sol
+++ Marketplace.sol
@@ -19,6 +19,7 @@ contract EscrowState {
 }
 
 contract Marketplace {
+    // this must be the point where we remove the right of the owner to transfer the ERC721
     function deposit(address nonFungibleTokenID) public {
       
       // Transfer reserveAsset amount to this contract.
@@ -50,8 +51,7 @@ contract Marketplace {
     
     
     // this function is the action that a lender can take when he wants to get the NFT transfered from the contract to his wallet
-    // this must be the point where we remove the right of the owner to transfer the ERC721
-    function lease(address nonFungibleTokenID) public {
+    function rentOut(address nonFungibleTokenID) public {
     
     }
     
